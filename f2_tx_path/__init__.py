8472dddc5c4c1e66ff36b9f59177f4a5eb0bec9a
Marko Kosunen <marko.kosunen@aalto.fi>
2019-01-17 11:27:26 -0800

    Update to thesdk v1.1

--- f2_tx_path/__init__.py
+++ f2_tx_path/__init__.py
@@ -58,10 +58,10 @@ class f2_tx_path(verilog,thesdk):
         self.iptr_A =[ refptr for i in range(self.Users) ] 
         self.user_weights      = [ (1+0j) for i in range(self.Users) ]
         #Thermometer and binary weighted outputs
-        self._Z_real_t= refptr()
-        self._Z_real_b= refptr()
-        self._Z_imag_t= refptr()
-        self._Z_imag_b= refptr()
+        self._Z_real_t= IO()
+        self._Z_real_b= IO()
+        self._Z_imag_t= IO()
+        self._Z_imag_b= IO()
 
         self.init()
     
@@ -95,7 +95,7 @@ class f2_tx_path(verilog,thesdk):
         if self.model=='py':
             self.process_input()
         elif self.model=='sv':
-            self.print_log({'type':'F','msg':'Verilog model not available'})
+            self.print_log(type='F', msg='Verilog model not available')
             #self.write_infile()
             ##Object to have handle for it in other methods
             ##Should be handled with a selector method using 'file' attribute
@@ -106,13 +106,13 @@ class f2_tx_path(verilog,thesdk):
             #[ _.remove() for _ in self.iofiles ]
 
     def process_input(self):
-        weighted_users=[ self.iptr_A[i].Value*self.user_weights[i] for i in range(self.Users) ]
+        weighted_users=[ self.iptr_A[i].Data*self.user_weights[i] for i in range(self.Users) ]
         if self.user_sum_mode==1:
             userssum=reduce(lambda prev, next: prev+next,weighted_users )
         else:
             userssum=weighted_users[self.user_select_index]
         #Process the data
-        self.interpolator.iptr_A.Value=userssum
+        self.interpolator.iptr_A.Data=userssum
         self.interpolator.run()
         self.segment_output()
 
@@ -120,9 +120,9 @@ class f2_tx_path(verilog,thesdk):
     #def write_infile(self,**kwargs):
     #    for i in range(self.Users):
     #        if i==0:
-    #            indata=self.iptr_A.data[i].udata.Value.reshape(-1,1)
+    #            indata=self.iptr_A.data[i].udata.Data.reshape(-1,1)
     #        else:
-    #            indata=np.r_['1',indata,self.iptr_A.data[i].udata.Value.reshape(-1,1)]
+    #            indata=np.r_['1',indata,self.iptr_A.data[i].udata.Data.reshape(-1,1)]
     #    if self.model=='sv':
     #        #This adds an iofile to self.iiofiles list
     #        a=verilog_iofile(self,**{'name':'A','data':indata})
@@ -138,24 +138,24 @@ class f2_tx_path(verilog,thesdk):
     #    a=list(filter(lambda x:x.name=='Z',self.iofiles))[0]
     #    a.read(**{'dtype':'object'})
     #    for i in range(self.Txantennas):
-    #        self._Z_real_t[i].Value=a.data[:,i*self.Txantennas+0].astype('str').reshape(-1,1)
-    #        self._Z_real_b[i].Value=a.data[:,i*self.Txantennas+1].astype('int').reshape(-1,1)
-    #        self._Z_imag_t[i].Value=a.data[:,i*self.Txantennas+2].astype('str').reshape(-1,1)
-    #        self._Z_imag_b[i].Value=a.data[:,i*self.Txantennas+3].astype('int').reshape(-1,1)
+    #        self._Z_real_t[i].Data=a.data[:,i*self.Txantennas+0].astype('str').reshape(-1,1)
+    #        self._Z_real_b[i].Data=a.data[:,i*self.Txantennas+1].astype('int').reshape(-1,1)
+    #        self._Z_imag_t[i].Data=a.data[:,i*self.Txantennas+2].astype('str').reshape(-1,1)
+    #        self._Z_imag_b[i].Data=a.data[:,i*self.Txantennas+3].astype('int').reshape(-1,1)
 
     def segment_output(self):
         self.print_log({'type':'I',
                 'msg':'Normalizing output to %s bits before segmentation'%(self.Txbits)})
-        max=np.amax(np.maximum(np.abs(np.real(self.interpolator._Z.Value))
-            ,np.abs(np.imag(self.interpolator._Z.Value))))
+        max=np.amax(np.maximum(np.abs(np.real(self.interpolator._Z.Data))
+            ,np.abs(np.imag(self.interpolator._Z.Data))))
         # Normalize, shift to positive and quantize
-        norm=np.round((self.interpolator._Z.Value/max+(1+1j))/2*(2**self.Txbits-1))
+        norm=np.round((self.interpolator._Z.Data/max+(1+1j))/2*(2**self.Txbits-1))
 
         #Convert thermo to string
         real_t=np.floor(np.real(norm)/(2**self.bin)).astype('int') 
         imag_t=np.floor(np.imag(norm)/(2**self.bin)).astype('int')
-        self._Z_real_b.Value=np.remainder(np.real(norm),2**self.bin).astype('int')
-        self._Z_imag_b.Value=np.remainder(np.imag(norm),2**self.bin).astype('int')
+        self._Z_real_b.Data=np.remainder(np.real(norm),2**self.bin).astype('int')
+        self._Z_imag_b.Data=np.remainder(np.imag(norm),2**self.bin).astype('int')
 
         # Segment the thermopart 
         segment=np.zeros((len(real_t),2**(self.Txbits-self.bin)-1)).astype('int')
@@ -163,13 +163,13 @@ class f2_tx_path(verilog,thesdk):
             if real_t[i,0] > 0:
                 segment[i,-real_t[i,0]:]=np.ones((1,real_t[i,0])).astype('int')
 
-        self._Z_real_t.Value=np.sum(segment.astype('str').astype(np.object),axis=1).astype('str').reshape(-1,1)
+        self._Z_real_t.Data=np.sum(segment.astype('str').astype(np.object),axis=1).astype('str').reshape(-1,1)
 
         segment=np.zeros((len(real_t),2**(self.Txbits-self.bin)-1)).astype('int')
         for i in range(len(imag_t)):
             if imag_t[i,0] > 0:
                 segment[i,-imag_t[i,0]:]=np.ones((1,imag_t[i,0])).astype('int')
-        self._Z_imag_t.Value=np.sum(segment.astype('str').astype(np.object),axis=1).astype('str').reshape(-1,1)
+        self._Z_imag_t.Data=np.sum(segment.astype('str').astype(np.object),axis=1).astype('str').reshape(-1,1)
         del segment
         del real_t
         del imag_t
